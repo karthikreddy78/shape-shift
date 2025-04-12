6aaf3cd2d77ef90fa5b385c77e42bde420a5fe2d
Karthik Reddy Musku <mkarthikreddy78@gmail.com>
2025-04-12 17:25:33 -0400

    Update look for menu page

--- app/src/app/menu/page.tsx
+++ app/src/app/menu/page.tsx
@@ -3,7 +3,7 @@ import SvgCard from "../_components/imports/svg-upload";
 export default function Menu() {
   return (
     <div className="flex h-screen flex-col items-center justify-center space-y-6 bg-[radial-gradient(ellipse_156.03%_212.89%_at_93.68%_-5.52%,_#766251_0%,_#262013_35%,_#030303_77%,_#2F2817_100%)]">
-      <h1 className="font-['Instrument Sans'] text-5xl leading-[59px] font-bold text-[#F3B518] not-italic">
+      <h1 className="font-['Instrument Sans'] text-5xl leading-[59px] font-bold text-[#FFFFFF] not-italic">
         Upload Your File
       </h1>
       <SvgCard />
