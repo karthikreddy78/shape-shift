2ba1cdfa7753634b65d25d60f505e62e331e0035
Karthik Reddy Musku <mkarthikreddy78@gmail.com>
2025-04-11 20:54:59 -0400

    first commit

---
+++ app/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "~/components/ui/button";
+export default async function Home() {
+  return (
+    <>
+      <div className="flex items-center justify-center">
+        <Button> Enter Playground</Button>
+      </div>
+    </>
+  );
+}
