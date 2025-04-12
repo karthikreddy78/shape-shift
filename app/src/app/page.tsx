215507ba29b9cb42153549470d963ca5c8b95742
Karthik Reddy Musku <mkarthikreddy78@gmail.com>
2025-04-12 17:16:47 -0400

    update bg

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default async function Home() {
   return (
     <>
-      <div className="flex h-screen w-full items-center justify-start pl-30 space-x-64 bg-[radial-gradient(ellipse_156.03%_212.89%_at_93.68%_-5.52%,_#867A5B_0%,_#2F2817_50%,_#030303_85%,_#2F2817_100%)]">
+      <div className="flex h-screen w-full items-center justify-start pl-30 space-x-64 bg-[radial-gradient(ellipse_156.03%_212.89%_at_93.68%_-5.52%,_#766251_0%,_#262013_35%,_#030303_70%,_#242015_93%)]">
         <div className="flex flex-col items-center justify-center space-y-2 space-x-12 md:w-1/2 md:items-start">
           <p className="font-instrumentsans text-center text-4xl leading-tight font-bold text-white md:text-left md:text-6xl md:leading-[84px] lg:text-[65px] max-w-[550px] mb-[20px]">
             Take your creation to the next dimension.
