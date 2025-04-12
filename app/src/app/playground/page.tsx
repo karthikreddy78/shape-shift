fabb60f90c52f50972173075a36cf30c73eaca28
Karthik Reddy Musku <mkarthikreddy78@gmail.com>
2025-04-12 13:03:11 -0400

    add suspense

--- app/src/app/playground/page.tsx
+++ app/src/app/playground/page.tsx
@@ -10,6 +10,7 @@ import {
 } from "~/components/ui/card";
 import { SidebarProvider } from "~/components/ui/sidebar";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 import { AppSidebar } from "~/components/app-sidebar";
 import { useState } from "react";
@@ -29,39 +30,44 @@ export default function Page() {
   };
 
   return (
-    <div className="flex items-center justify-center">
-      <SidebarProvider>
-        <div className="w-1/4">
-          <AppSidebar settings={settings} />
-        </div>
-        <Tabs defaultValue="React" className="w-full bg-[#030303]">
-          <TabsList className="bg-[#030303] text-[#F3B518]">
-            <TabsTrigger className="bg-[#030303] text-[#F3B518]" value="React">
-              React
-            </TabsTrigger>
-            <TabsTrigger
-              value="Vanilla"
-              className="bg-[#030303] text-[#F3B518]"
-            >
-              Vanilla
-            </TabsTrigger>
-          </TabsList>
-          <TabsContent value="React">
-            <Card className="border-[#F3B518] bg-[#030303]">
-              <CardHeader className="bg-[#030303] text-[#F3B518]">
-                <CardTitle>Playground</CardTitle>
-                <CardDescription className="text-[#F3B518]">
-                  Make changes to your react component export when done.
-                </CardDescription>
-              </CardHeader>
-              <CardContent className="space-y-2">
-                <ReactPlayground settings={settings} />
-              </CardContent>
-            </Card>
-          </TabsContent>
-          <TabsContent value="Vanilla">soon</TabsContent>
-        </Tabs>
-      </SidebarProvider>
-    </div>
+    <Suspense>
+      <div className="flex items-center justify-center">
+        <SidebarProvider>
+          <div className="w-1/4">
+            <AppSidebar settings={settings} />
+          </div>
+          <Tabs defaultValue="React" className="w-full bg-[#030303]">
+            <TabsList className="bg-[#030303] text-[#F3B518]">
+              <TabsTrigger
+                className="bg-[#030303] text-[#F3B518]"
+                value="React"
+              >
+                React
+              </TabsTrigger>
+              <TabsTrigger
+                value="Vanilla"
+                className="bg-[#030303] text-[#F3B518]"
+              >
+                Vanilla
+              </TabsTrigger>
+            </TabsList>
+            <TabsContent value="React">
+              <Card className="border-[#F3B518] bg-[#030303]">
+                <CardHeader className="bg-[#030303] text-[#F3B518]">
+                  <CardTitle>Playground</CardTitle>
+                  <CardDescription className="text-[#F3B518]">
+                    Make changes to your react component export when done.
+                  </CardDescription>
+                </CardHeader>
+                <CardContent className="space-y-2">
+                  <ReactPlayground settings={settings} />
+                </CardContent>
+              </Card>
+            </TabsContent>
+            <TabsContent value="Vanilla">soon</TabsContent>
+          </Tabs>
+        </SidebarProvider>
+      </div>
+    </Suspense>
   );
 }
